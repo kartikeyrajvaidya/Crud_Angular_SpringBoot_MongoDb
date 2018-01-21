6cc7127f20b5f0209cd07554ae5813e7686e6b53
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-21 22:19:48 +0530

    springBoot Project added using spring initializer

---
+++ simpleApp/src/main/java/com/springBootApp/simpleApp/SimpleAppApplication.java
@@ -0,0 +1,12 @@
+package com.springBootApp.simpleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppApplication.class, args);
+	}
+}
