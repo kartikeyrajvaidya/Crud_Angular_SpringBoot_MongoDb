014030ee419a7000f9eff61eb0548c00fbfd45b8
kartikey rajvaidya <kartikeyrajvaidya@gmail.com>
2018-01-22 13:21:28 +0530

    added student model: kartikey

---
+++ simpleApp/src/main/java/com/springBootApp/simpleApp/model/Student.java
@@ -0,0 +1,66 @@
+package com.springBootApp.simpleApp.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="student")
+public class Student {
+	
+	
+	@Id
+	int rollNo ;
+	
+	@NotBlank
+	String name;
+	
+	@NotBlank
+	@Size(max=100)
+	int percentage;
+	
+	@NotBlank
+	int attendence;
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
+
+	public int getAttendence() {
+		return attendence;
+	}
+
+	public void setAttendence(int attendence) {
+		this.attendence = attendence;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", percentage=" + percentage + ", attendence="
+				+ attendence + "]";
+	}
+	
+	
+
+}
